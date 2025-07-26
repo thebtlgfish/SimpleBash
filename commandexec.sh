16e6177a051093464a03876d9b684314e7f4a09d
Bootleg Fish <hypegaming.4k@gmail.com>
2025-07-26 13:58:10 -0400

    Add files via upload

---
+++ commandexec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This is a bash script that executes whatever command you want. Made by Bootleg Fish
+
+
+# The Script
+echo "hello and welcome to the command executor"
+sleep 1
+echo "Type the command you want to execute (Make sure it is a valid command)"
+
+read command
+
+sleep 1
+
+$command
